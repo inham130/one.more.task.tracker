772749f903d6ec7f2579f1ebeb39494265aff379
Andrey Frolov <inham130@gmail.com>
2022-06-01 20:56:53 +0300

    Hello React

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import styles from './app.css'
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>Hello React</h1>
+    </div>
+  );
+}
+export default App;
