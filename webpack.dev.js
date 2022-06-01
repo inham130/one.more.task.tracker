869c16674d3875ba4b792b82bf1879601e5d2c43
Andrey Frolov <inham130@gmail.com>
2022-06-01 20:56:25 +0300

    webpack comfiguration

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,17 +3,17 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        watchFiles: ['src/**/*.tsx', 'src/**/*.scss'],
-        compress: true,
-        hot: true,
-        open: true,
-        port: 3000,
-        historyApiFallback: true,
-    }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    watchFiles: ['src/**/*.tsx', 'src/**/*.ts', 'src/**/*.css'],
+    compress: true,
+    hot: true,
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,10 +3,10 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'source-map',
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    }
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  }
 });
