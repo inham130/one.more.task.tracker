e5e5dc83ee6e6f771d58cabc7e3dfa03a36e9e33
Andrey Frolov <inham130@gmail.com>
2022-06-04 11:49:06 +0300

    create and see tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,48 @@
-import { FC } from 'react';
+import { FC, MouseEvent, ChangeEvent, useState } from 'react';
 import styles from './app.css'
 
+type Task = {
+  id: number;
+  title: string;
+  description: string;
+}
+
 const App: FC = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDesctiorion] = useState('');
+  const [tasks, setTasks] = useState<Array<Task>>([]);
+
+  const handleCreateTask = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setTasks([...tasks, {id: new Date().getTime(),title, description}]);
+    setTitle('');
+    setDesctiorion('');
+  }
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  }
+  const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDesctiorion(event.target.value);
+  }
+
   return (
-    <div>
-      <h1 className={styles.h1}>Hello React</h1>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.h1}>Hello Tasks</h1>
+
+      <form className={styles.form}>
+        <input className={styles.input} onChange={handleTitleChange} value={title} type="text" name="title"/>
+        <input className={styles.input} onChange={handleDescriptionChange} value={description} type="text" name="description"/>
+        <button onClick={handleCreateTask}>Create Task</button>
+      </form>
+      <div className="task-list">
+        {tasks.map((task) => (
+            <div key={task.id} className="task">
+              <div className="task_title">{task.title}</div>
+              <div className="task_description">{task.description}</div>
+            </div>
+        ))}
+      </div>
+    </main>
   );
 }
 export default App;
