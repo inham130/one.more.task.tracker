af4ae98bb0c8c182d3c7fba7a17832c92b1f114b
Andrey Frolov <inham130@gmail.com>
2022-07-23 11:22:56 +0300

    Add modal to the app

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, FC, ReactNode, SetStateAction } from 'react';
 import styles from './modal.module.css';
+import { classNames } from '@utils/index';
 
 type OwnProps = {
   children?: ReactNode | string;
@@ -12,13 +13,10 @@ export const Modal: FC<OwnProps> = ({
   isVisible= false,
   setVisible
 }) => {
-  const rootClasses = [styles.modal];
-  if (isVisible) {
-    rootClasses.push(styles.modal_active);
-  }
+  const rootClasses = classNames(styles.modal, {[styles.modal_active]: isVisible});
 
   return (
-    <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+    <div className={rootClasses} onClick={() => setVisible(false)}>
       <div className={styles.modal__content} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
