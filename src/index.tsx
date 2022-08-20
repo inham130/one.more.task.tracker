4699502ea30365fc51ac4492bd7e9292967f574d
Andrey Frolov <inham130@gmail.com>
2022-08-20 19:14:31 +0300

    Redux store is configured

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import * as ReactDOM from 'react-dom';
-import { App } from './components/App'
+import { App } from '@components/App';
+import { store } from '@store/index';
+import { Provider } from 'react-redux';
 import './styles.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
